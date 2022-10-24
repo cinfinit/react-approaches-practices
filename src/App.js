f7b96bc377a5c442e47b85667efe376d11f65b16
rajagopal <rajagopal.sarathi@gmail.com>
2022-10-24 14:00:58 +0530

    Complete route navigation with pet images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import ChoosePet from './pages/pets/choosePet'
 import Home from './Home';
+import ViewPet from './pages/pets/viewPet';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pets" element={<ChoosePet />} />
+        <Route path="/pets/view" element={<ViewPet />} />
       </Routes>
   );
 }
--- src/pages/pets/choosePet.js
+++ src/pages/pets/choosePet.js
@@ -1,37 +1,46 @@
 import { useState } from 'react';
 import useRequest  from '../../hooks/useRequest';
-
+import { Link } from 'react-router-dom';
 
 const style = {
     position:"absolute",
     top: "50%",
     left:"50%",
-    width : "200px",
-    height:"200px",
+    width : "400px",
+    height: "300px",
     marginLeft:"-100px",
     marginTop:"-100px"
 }
+const flex ={
+    display: 'flex',
+    justifyContent: 'space-between'
+};
 
 function ChoosePet() {
     const { data, error, loading} = useRequest('https://dog.ceo/api/breeds/list/all');
-    const [dog, setDog] = useState("akita");
+    const [breed, setBreed] = useState("akita");
     let dogs = [];
     if(!loading && !error){
         dogs = Object.keys(data.message).filter(dog => data.message[dog].length <= 0);
         const subbreed = Object.keys(data.message).filter(dog => data.message[dog].length > 0);
-        subbreed.forEach(b => data.message[b].forEach(sb => dogs.push(sb +" "+ b)));
+        subbreed.forEach(b => data.message[b].forEach(sb => dogs.push(b+"/"+sb)));
     }
     return (
         <div style= {style} >
             { loading ? 
             (
             <div> Loading...</div>) :
-            <div>
-                <label htmlFor="dogs">Choose a breed:</label>
-                <select name="dogs" id="dogs" value={dog} onChange= {(evt) => setDog(evt.target.value)}>
-                    {dogs.map(dog => ( <option key={dog} value={dog}>{dog}</option>))}
-                </select>
-            </div> }
+            <div style={{display:'flex',flexDirection:'column',  rowGap: '1em'}}>
+                <div style = {flex}>
+                    <label htmlFor="dogs">Choose a breed:</label>
+                    <select name="dogs" id="dogs" value={breed} onChange= {(evt) => setBreed(evt.target.value)}>
+                        {dogs.map(breed => ( <option key={breed} value={breed}>{breed}</option>))}
+                    </select>
+                </div> 
+                <Link to="/pets/view" state={{breed:breed}}>
+                    <button>View Pet</button>
+                </Link>
+            </div>}
         </div>
     );
 }
